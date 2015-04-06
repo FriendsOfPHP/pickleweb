10925b333b55c8280f8cee8c44e801ba55612f2c
Grégoire Pineau <lyrixx@lyrixx.info>
2015-04-06 13:10:50 +0200

    Remove dead code

--- web/index.php
+++ web/index.php
@@ -1,10 +1,8 @@
 <?php
+
 require __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../vendor/twig/twig/lib/Twig/Autoloader.php';
 
 // use PickleWeb\View\Twig;
-
-\Twig_Autoloader::register();
 
 $app = new \Slim\Slim(
 		['view' => new PickleWeb\View\Twig()
