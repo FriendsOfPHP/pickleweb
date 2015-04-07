663a5b5d63ca98e92efb596680b49216f25166b4
Pierre Joye <pierre.php@gmail.com>
2015-04-07 12:34:59 +0200

    little ugly hack for now, support github auth and user

--- src/Action/AuthGithub.php
+++ src/Action/AuthGithub.php
@@ -7,8 +7,25 @@ use \OAuth\Common\Http\Uri\Uri as Uri;
 use \OAuth\Common\Http\Uri\UriFactory as UriFactory;
 
 class AuthGithub {
+	protected $app;
+
+	function __construct(\Slim\Slim $app) {
+		$this->app = $app;	
+	}
+
 	function login() {
 		$storage = new Session();
+
+		$code = $this->app->request->get('code');
+		$go = $this->app->request->get('go');
+
+		if (empty($go) && empty($code)) {
+			$token = $storage->retrieveAccessToken('github')->getAccessToken();
+			if (!empty($token)) {
+				$this->app->redirect('/');
+			}
+		}
+
 		$servicesCredentials['github'] = [
 			'key' => getenv('GITHUB_CLIENT_ID'),
 			'secret' => getenv('GITHUB_CLIENT_SECRET')
@@ -18,24 +35,44 @@ class AuthGithub {
 		$currentUri = $uriFactory->createFromSuperGlobalArray($_SERVER);
 
 		$credentials = new Credentials(
-				$servicesCredentials['github']['key'],
-				$servicesCredentials['github']['secret'],
-				$currentUri->getAbsoluteUri()
+					$servicesCredentials['github']['key'],
+					$servicesCredentials['github']['secret'],
+					$currentUri->getAbsoluteUri()
 				);
 
 		$serviceFactory = new \OAuth\ServiceFactory();
 		$gitHub = $serviceFactory->createService('GitHub', $credentials, $storage, array('user:email', 'read:repo_hook'));
-		if (!empty($_GET['code'])) {
-			// This was a callback request from github, get the token
-			$gitHub->requestAccessToken($_GET['code']);
+		
+		if ($code) {
+			/* This was a callback request from github, get the token */
+			$token = $gitHub->requestAccessToken($code);
 			$result = json_decode($gitHub->request('user/emails'), true);
-			echo 'The first email on your github account is ' . $result[0];
-		} elseif (!empty($_GET['go']) && $_GET['go'] === 'go') {
-			$url = $gitHub->getAuthorizationUri();
-			header('Location: ' . $url);
+			
+			$storage->storeAccessToken('github', $token);
+
+			$result = json_decode($gitHub->request('user'), true);
+			$result['email'] = json_decode($gitHub->request('user/emails'), true)[0];
+
+			$this->app->view()->setData([
+				'github_content' => 'Github email: ' . $result['email'] . "\n" . 
+									'Username: ' . $result['login']
+			]);
+			$this->app->render('registergithub.html');
+			$_SESSION['user'] = $result;
+			$_SESSION['token'] = $token;
+			$jsonPath = $this->app->config('json_path') . 'users/github/' . $result['login'] . '.json';
+
+			if (!file_exists($jsonPath)) {
+				file_put_contents($jsonPath, json_encode($result, JSON_PRETTY_PRINT));
+			}
+			header('Location: ' . /);
 		} else {
-			$url = $currentUri->getRelativeUri() . '?go=go';
-			echo "<a href='$url'>Login with Github!</a>";
+			if (!empty($go) && $go === 'go') {
+				$url = $gitHub->getAuthorizationUri();
+				header('Location: ' . $url);
+			} else {
+				$this->app->redirect('/');
+			}
 		}
 	}
 }
