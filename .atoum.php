a90eecb470efd06e0d4b18203cb602f334081360
jubianchi <julien.bianchi@pmsipilot.com>
2015-04-13 11:38:04 +0200

    Fix atoum config and add travis one

--- .atoum.php
+++ .atoum.php
@@ -4,8 +4,6 @@ use PickleWeb\Tests\Atoum\Report\Fields\Runner\Pickle;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$script->addTestsFromDirectory(__DIR__ . '/tests/unit');
 $script->noCodeCoverageForNamespaces('Slim');
-
-$script
-    ->addDefaultReport()
-    ->addField(new Pickle());
+$script->addDefaultReport()->addField(new Pickle());
