0dcb48db7420ffb3fdba3507264f09949cc83d73
Pierre Joye <pierre.php@gmail.com>
2015-04-21 05:54:09 +0200

    CS

--- src/Package.php
+++ src/Package.php
@@ -4,117 +4,116 @@ namespace PickleWeb;
 
 class Package implements \Serializable
 {
-	
-	protected $name;
-	protected $tag;
+    protected $name;
+    protected $tag;
 
-	protected $keysExport = [
-		'name',
-		'version',
-		'version_normalized',
-		'type',
-		'stability',
-		'license',
-		'homepage',
-		'authors',
-		'description',
-		'time',
-		'support',
-		'source',
-		'dist'
-	];
-	protected $values = [];
-	
-	public function __construct()
-	{
-		foreach ($this->keysExport as $key) {
-				$this->values[$key] = '';
-		}
-	}
+    protected $keysExport = [
+        'name',
+        'version',
+        'version_normalized',
+        'type',
+        'stability',
+        'license',
+        'homepage',
+        'authors',
+        'description',
+        'time',
+        'support',
+        'source',
+        'dist',
+    ];
+    protected $values = [];
 
-	public function setFromArray($data)
-	{
+    public function __construct()
+    {
+        foreach ($this->keysExport as $key) {
+            $this->values[$key] = '';
+        }
+    }
+
+    public function setFromArray($data)
+    {
         foreach ($data as $key => $value) {
             if (isset($this->values[$key])) {
                 $this->values[$key] = $value;
             }
         }
-	}
-	
-	public function getAsArray()
-	{
-		return $this->values;
-	}
+    }
 
-	public function setName($name)
-	{
-		$this->name = $name;
-	}
+    public function getAsArray()
+    {
+        return $this->values;
+    }
 
-	public function getName()
-	{
-		return $this->name;
-	}
-	
-	public function setTag($tag)
-	{
-		$this->tag = $tag;
-	}
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 
-	public function getTag()
-	{
-		return $this->tag;
-	}
-	
-	public function getDescription()
-	{
-		return $this->values['description'];
-	}
-	
-	public function getLicense()
-	{
-		return $this->values['license'];
-	}
+    public function getName()
+    {
+        return $this->name;
+    }
 
-	public function getAuthors()
-	{
-		return $this->values['authors'];
-	}
-	
-	public function getVersion()
-	{
-		return $this->values['version'];
-	}
+    public function setTag($tag)
+    {
+        $this->tag = $tag;
+    }
 
-	public function getVersionNormalized()
-	{
-		return $this->values['version_normalized'];
-	}
+    public function getTag()
+    {
+        return $this->tag;
+    }
 
-	public function getTime()
-	{
-		return $this->values['time'];
-	}
+    public function getDescription()
+    {
+        return $this->values['description'];
+    }
 
+    public function getLicense()
+    {
+        return $this->values['license'];
+    }
 
-	public function getId()
-	{
-		return $this->values['source']['reference'];
-	}
+    public function getAuthors()
+    {
+        return $this->values['authors'];
+    }
 
-	public function serialize()
-	{
-		$data = [
-			[
-				$this->tag => $this->values
-			]
-		];
-		return json_encode($data);
-	}
-	
-	public function unserialize($serialized)
-	{
-		$data = json_decode($serialized, true);
-		$this->setFromArray($data);
-	}
+    public function getVersion()
+    {
+        return $this->values['version'];
+    }
+
+    public function getVersionNormalized()
+    {
+        return $this->values['version_normalized'];
+    }
+
+    public function getTime()
+    {
+        return $this->values['time'];
+    }
+
+    public function getId()
+    {
+        return $this->values['source']['reference'];
+    }
+
+    public function serialize()
+    {
+        $data = [
+            [
+                $this->tag => $this->values,
+            ],
+        ];
+
+        return json_encode($data);
+    }
+
+    public function unserialize($serialized)
+    {
+        $data = json_decode($serialized, true);
+        $this->setFromArray($data);
+    }
 }
